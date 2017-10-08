4b3f110233ab687de2f80e8bddd7f6dda269a106
PixelMaker <fernandonavarijo1996@gmail.com>
2017-10-08 11:10:03 -0600

    Readme of the project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Temperature Project
+Este es el webserver para registrar los datos lanzados por el hadware.
+
+Para poder correr el proyecto es necesario solamente tener instalado [docker](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04). De preferencia si es en Ubuntu (Presenta menos problemas).
+
+## Ejecucion
+---
+Para poder levantar el proyecto, se debe correr el siguiente comando dentro de la carpeta.
+```
+yarn temp:start
+```
+
+### Dudas
+---
+Puede ser necesaria la instalacion de [yarn](https://yarnpkg.com/lang/en/docs/install/)
--- routes/temp.js
+++ routes/temp.js
@@ -1,19 +1,49 @@
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const multer = require('multer');
-const upload = multer();
+const multer = require('multer')();
+
+const DB_TABLE = 'temperature';
+
+const nano = require('nano')('http://docker_couch_1:5984');
+nano.db.create(DB_TABLE);
+const tempTable = nano.db.use(DB_TABLE);
 
 express().use(bodyParser.json());
 express().use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', (req, res, next) => {
-    res.send('Getting a temperature');
+    tempTable.list({include_docs: true}, (err, body) => {
+        if (err) res.send('err');
+        else{
+            const result = _.map(body.rows, 'doc');
+            res.send(result);
+        } 
+    });
 });
 
-router.post('/', upload.array(), (req, res, next) => {
-    console.log(req.body);
-    res.send('saving');
+router.get('/shouldTurnOn', (req, res, next) => {
+    nano.db.changes(DB_TABLE, { descending: true, limit: 1 }, (err, body) => {
+        const lastId = body.results.length > 0 ? body.results[0].id : null;
+        if (!err) {
+            tempTable.get(lastId, (errId, bodyDoc) => {
+                if (!errId) res.send(bodyDoc);
+            });
+        }
+    })
+});
+
+router.post('/', multer.array(), (req, res, next) => {
+    const { fecha, temperature } = req.body;
+    tempTable.insert({ fecha, temperature }, null, (err, body) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.send('Stored temperature');
+        } 
+    });
 });
 
 module.exports = router;
